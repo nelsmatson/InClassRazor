c0d1a15a4c9d02549b6d0116a7221ca20b51c904
nelsmatson <ihavegumption@gmail.com>
2021-05-31 16:50:54 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using InClassRazor.Models;
+
+namespace InClassRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+        public IActionResult IndexWithLayout()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            GuestContact contact = new GuestContact();
+
+            contact.Name = "Nels Matson";
+            contact.Email = "nelsmatson@usf.edu";
+            contact.Phone = "813-123-4567";
+
+
+
+
+            return View(contact);
+        }
+
+        [HttpPost]
+        public IActionResult Contact(GuestContact contact)
+        {
+            return View(contact);
+        }
+    }
+}
+    
+        
+   
+
---
+++ Models/App_Models.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InClassRazor.Models
+{
+    public class GuestContact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class ChartModel
+    {
+        public string ChartType { get; set; }
+        public string Labels { get; set; }
+        public string Data { get; set; }
+        public string Title { get; set; }
+    }
+
+}
+	
+
